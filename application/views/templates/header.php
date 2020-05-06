31edf7f88045944246051892a642b47f60e61eff
Geng Julián <geng.julian@gmail.com>
2020-05-06 20:52:33 +0200

    Creating posts model. Styling posts view.

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,6 +2,7 @@
 	<head>
 		<title>Header</title>
 		<link type="text/css" rel="stylesheet" href="https://bootswatch.com/4/lux/bootstrap.min.css">
+		<link type="text/css" rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css" >
 	</head>
 	<body>
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -17,6 +18,9 @@
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="<?php echo base_url();?>about">About</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="<?php echo base_url();?>posts">Posts</a>
 				</li>
 			</ul>
 			<form class="form-inline my-2 my-lg-0">
